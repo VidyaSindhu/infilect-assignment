708f37d71adc6af9cbaeab494286b753007e1d18
VidyaSindhu <vidyasd08@gmail.com>
2022-05-09 09:01:08 +0530

    added multithreading feature that will fetch weather details from third party api in every 30 mins

--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
 import json
+from threading import Thread
+from time import sleep
+import redis
+from redis.commands.json.path import Path
 from rest_framework.pagination import PageNumberPagination
 from django.forms import ValidationError
 from django.core.exceptions import ValidationError
@@ -12,7 +16,10 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.hashers import check_password
 from rest_framework.authentication import TokenAuthentication
 import requests
+from django.core.paginator import Paginator
 # Create your views here.
+
+CITIES = ['Gwalior', 'London', 'New York', 'New Delhi', 'Mumbai', 'Kolkata', 'Chennai', 'Pune', 'Indore', 'Bhopal', 'Jabalpur', 'Surat', 'Varanasi', 'Allahabad', 'Lucknow', 'Chandigarh', 'Noida', 'Tokyo', 'California', 'Paris', 'Jaipur', 'Patna', 'Ranchi', 'Dhanbad', 'Haridwar', 'Hyderabad', 'Kochi', 'Goa', 'Rome', 'Munich']
 class TokenAuthentication(TokenAuthentication):
     TokenAuthentication.keyword = "Bearer"
 
@@ -61,19 +68,32 @@ def user_logout(request):
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def get_weather_request(request):
+def get_weather_details(request):
     page = int(request.GET.get('page'))-1
-    URL = "https://api.openweathermap.org/data/2.5/weather"
-    PARAMS = {'appid':'432a2784d5ffb660d8a21fc122b8eed8'}
-    cities = ['Gwalior', 'London', 'New York', 'New Delhi', 'Mumbai', 'Kolkata', 'Chennai', 'Pune', 'Indore', 'Bhopal', 'Jabalpur', 'Surat', 'Varanasi', 'Allahabad', 'Lucknow', 'Chandigarh', 'Noida', 'Tokyo', 'California', 'Paris']
+    
     res = []
-    for i in range (10*page, 10*page+10):
-        PARAMS['q'] = cities[i]
-        r = requests.get(url= URL, params= PARAMS)
-        res.append(r.json())
-    print(len(res))
-    paginator = PageNumberPagination()
-    paginator.page_size = 10
-    # return paginator.get_paginated_response(res)
-    return Response(data = res)
+    redis_client = redis.Redis(host='localhost', port=6379, db=0)
+    cities = []
+    for key in redis_client.scan_iter():
+        cities.append(key)
+        res.append(redis_client.json().get(key))
+    print(cities)
+    redis_client.close()
+    return Response(data = res[10*page:10*page+10])
 
+class WeatherRequest(Thread):
+    def run(self):
+        redis_client = redis.Redis(host='localhost', port=6379, db=0)
+        URL = "https://api.openweathermap.org/data/2.5/weather"
+        PARAMS = {'appid':'432a2784d5ffb660d8a21fc122b8eed8'}
+        while(True):
+            for city in CITIES:
+                PARAMS['q'] = city
+                r = requests.get(url= URL, params= PARAMS)
+                redis_client.json().set(str(city), Path.root_path(), r.json())
+            redis_client.close()
+            
+            sleep(30*60)
+
+weather_details = WeatherRequest()
+weather_details.start()
